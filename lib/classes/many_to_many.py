11c7f1cc14607bbc76158c5afbc5f9104ae48ada
Kyle <kylejrgurlad@gmail.com>
2024-06-03 10:02:40 +0300

    week-2-code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,97 +1,78 @@
 class Article:
     all = []
-
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self._title = title  # Use the private attribute to allow setting the title initially
+        self._title = title
         Article.all.append(self)
-
+        
     @property
     def title(self):
         return self._title
 
-    @title.setter
-    def title(self, value):
-        raise AttributeError("Cannot set attribute 'title'")
-
-    
 
 class Author:
     def __init__(self, name):
-        if not isinstance(name, str):
-            raise ValueError("Name must be a string")
+        if not isinstance(name,str):
+            raise ValueError("Name is of type string")
         if len(name) == 0:
-            raise ValueError("Name must have more than 0 characters")
-        
+            raise ValueError("Name cannot have 0 characters")
+
         self._name = name
         self._articles = []
         self._magazines = []
-        
-    
+
     @property
     def name(self):
         return self._name
 
-    @name.setter
-    def name(self,value):
-        raise ValueError("Cannot change authors name")
-
-
     def articles(self):
         return [article for article in Article.all if article.author == self]
-        
 
     def magazines(self):
         return list(set(article.magazine for article in Article.all if article.author == self))
 
     def add_article(self, magazine, title):
-        return Article(self, magazine, title)
+        return Article(self,magazine,title)
 
     def topic_areas(self):
-       categories = {article.magazine.category for article in Article.all if article.author == self}
-       return list(categories) if categories else None
-    
-from collections import defaultdict
+        categories = {article.magazine.category for article in Article.all if article.author == self}
+        return list(categories) if categories else None
 
+        
 class Magazine:
-    def __init__(self, name: str, category: str):
-        self._validate_name_and_category(name, category)
-        self.name = name
-        self.category = category
-
-    def _validate_name_and_category(self, name: str, category: str):
-        if not isinstance(name, str) or not isinstance(category, str):
-            raise ValueError("Name and category must be strings")
-        if not 2 <= len(name) <= 16:
-            raise ValueError("Name must be between 2 and 16 characters long")
+    def __init__(self, name, category):
+        if not isinstance(name,str):
+            raise ValueError("Name is of type string")
+        if not (2 <= len(name) <= 16):
+            raise Exception("Name must be between 2 and 16 characters")
+        if not isinstance(name,str):
+            raise ValueError("Category is of type string")
         if not category:
             raise ValueError("Category cannot be empty")
 
+        self.name = name
+        self.category = category
+
     def articles(self):
-        return [article for article in Article.all if article.magazine == self]
+         return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
         return list(set(article.author for article in Article.all if article.magazine == self))
 
     def article_titles(self):
         titles = [article.title for article in Article.all if article.magazine == self]
-        return titles if titles else []
+        return titles if titles else None
 
     def contributing_authors(self):
-        author_article_count = defaultdict(int)
+        author_count = {}  # Dictionary to store the count of articles for each author
         for article in self.articles():
-            author_article_count[article.author.name] += 1
-        return [author for author in set(article.author for article in self.articles()) 
-                if author_article_count[author.name] > 2]
+            author = article.author
+            if author in author_count:
+                author_count[author] += 1
+            else:
+                author_count[author] = 1
 
-
-author_1 = Author("Carry Bradshaw")
-author_2 = Author("Nathaniel Hawthorne")
-magazine_1 = Magazine("Vogue", "Fashion")
-magazine_2 = Magazine("AD", "Architecture")
-Article(author_1, magazine_1, "How to wear a tutu with style")
-Article(author_1, magazine_1, "How to be single and happy")
-Article(author_1, magazine_1, "Dating life in NYC")
-Article(author_1, magazine_2, "Carrara Marble is so 2020")
-Article(author_2, magazine_2, "2023 Eccentric Design Trends")
+        # Filter authors who have written more than 2 articles
+        contributing_authors = [author for author, count in author_count.items() if count > 2]
+        return contributing_authors
